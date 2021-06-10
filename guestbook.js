a98558e312ebcf0589e10fa60e418e74e4a5410b
Unknown-CloudDev <83724355+Unknown-CloudDev@users.noreply.github.com>
2021-06-10 11:56:40 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://58e67c3a.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
